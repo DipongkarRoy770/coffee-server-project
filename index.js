62946276ced2dcbefbf697c59c61ccbaf3a552bf
Dipongkar Roy <dipongkarroy233@gmail.com>
2024-02-18 14:40:52 +0600

    data delete done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 //midleWere:
 app.use(cors())
 app.use(express.json())
- 
+
 const uri = `mongodb+srv://${process.env.coffee_Db}:${process.env.coffee_pass}@cluster0.bqchovi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,30 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const database = client.db("CoffeeDB");
+        const coffeeConection = database.collection("coffee");
+
+
+        app.get('/coffee', async (req, res) => {
+            const cursor = coffeeConection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/coffee', async (req, res) => {
+            const coffee = req.body;
+            const result = await coffeeConection.insertOne(coffee)
+            res.send(result)
+
+        })
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            console.log(query)
+            const result = await coffeeConection.deleteOne(query)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
